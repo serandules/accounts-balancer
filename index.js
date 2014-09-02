03b0d632f9f67fdf5b608e05f4fb25bee0801da8
ruchiraw <ruchira.wageesha@gmail.com>
2014-09-02 23:22:42 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var http = require('http');
+var express = require('express');
+var agent = require('hub-agent');
+
+var HTTP_PORT = 4002;
+
+var dev = process.env.NODE_ENV === 'development';
+
+var app = express();
+
+app.use(agent.proxy());
+
+agent(http.createServer(app, HTTP_PORT));
