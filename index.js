10258da1e3b172ffe1da6992f12fe9eea8704b08
ruchiraw <ruchira.wageesha@gmail.com>
2014-09-07 12:02:38 +0530

    updating

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ agent(http.createServer(app), HTTP_PORT);
 
 process.on('uncaughtException', function (err) {
     console.log('unhandled exception ' + err);
-    console.trace(err.stack);
+    console.log(err.stack);
 });
